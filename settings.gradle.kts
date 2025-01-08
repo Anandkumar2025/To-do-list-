f01e0756fc6dd5b33e8c4f830f9bc89aebedad85
Anand2025 <anandpramodyadav@gmail.com>
2025-01-08 22:52:38 +0530

    This a to do list app

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,21 +5,16 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
-//dependencyResolutionManagement {
-//    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-//    repositories {
-//        google()
-//        mavenCentral()
-//    }
-//}
+
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        google() // Required for Android plugins
+        google()
         mavenCentral()
     }
 }
+
 
 rootProject.name = "My Application1"
 include(":app")
